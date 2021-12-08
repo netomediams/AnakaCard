f1e430458152749cb37f1aa6e476852a59e8421d
Kay Kostadinov <kaloyan.kostadinov@anakatech.com>
2021-12-08 15:31:49 +0200

    added AnakaCard component, design

--- src/views/AnakaCard.jsx
+++ src/views/AnakaCard.jsx
@@ -1,5 +1,8 @@
 import { Component, createRef } from 'react';
 import { toPng, toJpeg } from 'html-to-image';
+import thankYouV1 from '../resources/images/cards/Christmas_thank_you_card_v1_800x1075_nameless.jpg'
+import thankYouV2 from '../resources/images/cards/Christmas_thank_you_card_v2_800x1075_nameless.jpg'
+import thankYouV3 from '../resources/images/cards/Christmas_thank_you_card_v3_800x1075_nameless.jpg'
 
 export default class AnakaCard extends Component {
 
@@ -7,17 +10,18 @@ export default class AnakaCard extends Component {
     super(props);
     this.nodeRef = createRef();
     this.state = {
+      name: '',
       img: '',
-      image: null
+      selected: thankYouV1,
     }
   }
 
 
+
   getImage = async () => {
-    console.log('fired')
     try {
       console.log(this.nodeRef)
-      const dataURL = await toPng(this.nodeRef.current);
+      const dataURL = await toJpeg(this.nodeRef.current);
       const imageInstance = new Image();
       imageInstance.src = dataURL;
       
@@ -28,22 +32,39 @@ export default class AnakaCard extends Component {
   }
 
 
+  handleChangeTemplate = (template) => {
+    this.setState({selected: template})
+  }
+
+
   render() {
     return (
-      <div className="homepage">
-          <a
-            ref={this.nodeRef}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{backgroundColor: '#282c34'}}
-          >
-            Learn React
-          </a>
-          <button onClick={this.getImage}>make image</button>
-          <img src={this.state.img} alt='image here' />
-          {this.image && this.image}
+      <div className="anakacard">
+          <div className="card_config">
+            <h2>Select template</h2>
+            <div className="template_picker">
+              <img src={thankYouV1} alt="" onClick={() => this.handleChangeTemplate(thankYouV1)} className="img-fluid" />
+              <img src={thankYouV2} alt="" onClick={() => this.handleChangeTemplate(thankYouV2)} className="img-fluid" />
+              <img src={thankYouV3} alt="" onClick={() => this.handleChangeTemplate(thankYouV3)} className="img-fluid" />
+            </div>
+            <div className="controls">
+              <label>Name</label>
+              <input type="text" onChange={e => this.setState({name: e.target.value})} />
+              <div className="generate">
+                <h2>Generate image</h2>
+                <div className="buttons">
+                  <button className='btn' onClick={this.getImage}>Download jpeg</button>
+                  <button className='btn' onClick={this.getImage}>Download png</button>
+                  <button className='btn' onClick={this.getImage}>Copy as html</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div className='card_container' ref={this.nodeRef}>
+            <h1 className='name'>{this.state.name}</h1>
+            <img src={this.state.selected} alt="" className='img-fluid' />
+          </div>
       </div>  
     );
   }
